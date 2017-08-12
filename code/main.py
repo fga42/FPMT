4dffef954bcea9ef08d67492ad1ea3413e3141ca
fga42 <f.gambert@gmail.com>
2017-08-12 15:35:14 +0100

    push first real commit

---
+++ code/main.py
@@ -0,0 +1,25 @@
+# desc: main file for FPMT
+# creation date: 12-08-2017
+#
+
+
+
+# connect to a source of market data to compare stocks' ratios
+def screen_stocks()
+#    --> ability to connect to yahoo finance/other data feed
+#    --> get list of stock to scan (from what page, what repository?)
+#     --> for each stock: get financial info 
+#	 --> compare to predefined ratio
+#		 --> print number of stocks matching the ratio defined
+#		 --> display stocks order from best result stock to lower
+
+
+
+def main():
+    print ("hello world")
+    screen_stocks()
+    # - how to build UI ?
+    
+	
+	
+main()	
